aff823209b6021c107f89d2301861d8011fe4a37
xenoxsite <xenoxsite@gmail.com>
2024-11-25 21:49:59 +0600

    Update config.h

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=20:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=20:antialias=true:autohint=true" };
+static char *font = "JetBrains Mono:pixelsize=21:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=21:antialias=true:autohint=true" };
 static int borderpx = 15;
 
 /*
